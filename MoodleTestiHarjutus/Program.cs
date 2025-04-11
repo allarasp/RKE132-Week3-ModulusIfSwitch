328b5c71d367d4df8c12d45669db9edb8dc17246
Allar Asper <allarasper@gmail.com>
2025-04-11 14:55:31 +0300

    update week3

---
+++ MoodleTestiHarjutus/Program.cs
@@ -0,0 +1,60 @@
+﻿//Mis hinne?
+
+Console.WriteLine("Mis hinde sa said?");
+
+char hinne = Convert.ToChar(Console.ReadLine().ToLower());
+
+
+if (hinne == 'a')
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if(hinne == 'b')
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (hinne == 'c')
+{
+    Console.WriteLine("Hea!");
+}
+else if (hinne == 'd')
+{
+    Console.WriteLine("Rahuldav!");
+}
+else if (hinne == 'e')
+{
+    Console.WriteLine("Kasin!");
+}
+else if (hinne == 'f')
+{
+    Console.WriteLine("Puudulik!");
+}
+else
+{
+    Console.WriteLine("Vale väärtus!");
+}
+//switch (hinne)
+//{
+//   case 'a':
+//       Console.WriteLine("Suurepärane!");
+//       break;
+//   case 'b':
+//       Console.WriteLine("Väga hea!");
+//       break;
+//   case 'c':
+//      Console.WriteLine("Hea!");
+//      break;
+//   case 'd':
+//     Console.WriteLine("Rahuldav!");
+//       break;
+//   case 'e':
+//       Console.WriteLine("Kasin!");
+//       break;
+//   case 'f':
+//       Console.WriteLine("Puudulik!");
+//       break;
+//
+//    default:
+//        Console.WriteLine("Vale väärtus!");
+//        break;
+//}
